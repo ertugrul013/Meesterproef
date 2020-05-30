57b35ee81395761caaaecd1060d9637f674bb8b9
ertugrul013 <ertugrul013@hotmail.com>
2020-05-31 01:05:13 +0200

    fixed api endpoint

--- API/functions/index.js
+++ API/functions/index.js
@@ -4,6 +4,22 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 
 var app = express();
+var transport = nodemailer.createTransport({
+    secure: false,
+    host: "smtp.live.com",
+    port: 587,
+    auth: {
+        user: process.env.user,
+        pass: process.env.pass
+    }
+});
+transport.verify(function (error, success) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Server is ready to take our messages");
+    }
+});
 
 
 const subjectText = ["Het linkje naar de verslagen", "this user downloaded ur paper"];
@@ -15,21 +31,23 @@ app.get('/', function (req, res) {
 })
 
 app.get('/api/download', function (req, res) {
-    console.log("User visited /api/download with a get request");
-    res.send("Wrong request try it with a post request ")
-})
-
-app.post('/api/downlaod', function (req, res) {
     if (req.query.invite_code == 1337) {
         res.json({ "result": "error" });
     }
 
     console.log("sending email to user");
-
+    SendEmail(
+        req.query.email,
+        subjectText[0] + req.query.name,
+        textBody[0]);
 
     console.log("Sending email to admin");
 
-
+    SendEmail(
+        process.env.admin,
+        subjectText[1] + req.query.email + req.query.name,
+        textBody[1],
+    );
     res.status(200);
     res.send("Success");
 })
