3aef2f3a0ad13d56271fce739d7bad2dc0cd5d17
ertugrul013 <ertugrul013@hotmail.com>
2020-06-01 16:36:11 +0200

    Added diferent path for teachert

--- API/functions/index.js
+++ API/functions/index.js
@@ -22,8 +22,15 @@ transport.verify(function (error, success) {
 });
 
 
-const subjectText = ["Het linkje naar de verslagen", "this user downloaded ur paper"];
-const textBody = ["34", "12"];
+const subjectText = ["Het linkje naar de verslagen voor ", "this user downloaded ur paper"];
+
+const textBody = ["Hierbij het linkje voor het verslag https://docs.google.com/document/d/1NCMDHVdh4mDi0YC5eoS3HKsIjBLaPI9NXGcgnhWjyfU/edit?usp=sharing",
+    "Hierbij het linkje voor het verslag https://drive.google.com/drive/folders/1M7jBvBcheGdQj4b8BaY5a7c70ePjTwEu?usp=sharing",
+    "this user downloaded ur files"];
+
+const HTMLBody = ["<h1><p>Hierbij het linkje voor het verslag</p><a href=https://docs.google.com/document/d/1NCMDHVdh4mDi0YC5eoS3HKsIjBLaPI9NXGcgnhWjyfU/edit?usp=sharing>linkje</a></h1>"
+    , "<h1><p>Hierbij het linkje voor het verslag</p><a href=https://drive.google.com/drive/folders/1M7jBvBcheGdQj4b8BaY5a7c70ePjTwEu?usp=sharing>linkje</a></h1>",
+    "<h1><p> this user downloaded ur files</p></h1><P>"]
 
 app.get('/', function (req, res) {
     res.status(200);
@@ -31,23 +38,44 @@ app.get('/', function (req, res) {
 })
 
 app.get('/api/download', function (req, res) {
-    if (req.query.invite_code != 1337) {
+    let isTeacher = false;
+    if (req.query.invite_code != 1337 && req.query.invite_code != 39687) {
         res.json({ "result": "error" });
         return;
+    } else {
+        if (req.query.invite_code = 39687) {
+            console.log("teacher accessed API");
+            isTeacher = true;
+        } else {
+            console.log("user accessed API");
+            isTeacher = false;
+        }
     }
 
-    console.log("sending email to user");
-    SendEmail(
-        req.query.email,
-        subjectText[0] + req.query.name,
-        textBody[0]);
+    if (isTeacher == false) {
+        SendEmail(
+            req.query.email,
+            subjectText[0] + req.query.name,
+            textBody[0]),
+            HTMLBody[0]
+        console.log("Email has been sent to user");
+    } else {
+        SendEmail(
+            req.query.email,
+            subjectText[0] + req.query.name,
+            textBody[1]),
+            HTMLBody[1]
+        console.log("Email has been sent to teacher");
+    }
 
     console.log("Sending email to admin");
 
     SendEmail(
         process.env.user,
-        subjectText[1] + req.query.email + req.query.name,
-        textBody[1],
+        subjectText[1],
+        textBody[2] + " " + req.query.email + " " + req.query.name + "is teacher: " + isTeacher,
+        HTMLBody[2] + " " + req.query.email + " " + req.query.name + "is teacher: " + isTeacher + "</p>"
+
     );
     res.status(200);
     res.json({ "result": "success" });
@@ -65,12 +93,13 @@ app.listen(3000, function () {
 })
 
 
-function SendEmail(_to, _subject, _text) {
+function SendEmail(_to, _subject, _text, _html) {
     const message = {
         from: process.env.user, // Sender address
         to: _to,         // List of recipients
         subject: _subject, // Subject line
-        text: _text// Plain text body
+        text: _text,// Plain text body,
+        html: _html
     };
     transport.sendMail(message, function (err, info) {
         if (err) {
